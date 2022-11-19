dfc556e20dfa8b96ebb1e1cb7ac10dfe2ab411aa
DELL <DELL@satya>
2022-11-19 14:39:24 +0530

    this is first jenkins demo

---
+++ src/main/java/mavenjenkins506/CSEMJDEMO506/App.java
@@ -0,0 +1,13 @@
+package mavenjenkins506.CSEMJDEMO506;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "WELCOME TO JENKINS WORLD" );
+    }
+}
